06b67526245d09007c532f867bea7f0f867cf408
21-01 <banyaklontelontemaudijual+git@gmail.com>
2023-11-06 03:42:03 +0700

    JavaScript 01-04 Assignments - and others

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(strings) {
+    var arrayOfString = strings.split("");
+    var reversedArray = arrayOfString.reverse();
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
