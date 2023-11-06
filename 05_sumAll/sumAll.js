ab697aee7c626f15841fb2ea0be898488d91e122
21-01 <banyaklontelontemaudijual+git@gmail.com>
2023-11-07 03:30:07 +0700

    JavaScript 05-07 Assignments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0) return "ERROR"
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
+    if (int1 > int2){
+        let tmp = int1
+        int1 = int2
+        int2 = tmp
+    }
 
+    var totalSum = 0;
+    int1--
+    for (i = int1; i <= int2; i++) {
+        totalSum = totalSum + i
+    };
+    return totalSum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  var converted = (fahrenheit - 32) * 5/9
+  return Math.round(converted * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  var converted = (celcius * 9/5) + 32
+  return Math.round(converted * 10) / 10
 };
+
 
 // Do not edit below this line
 module.exports = {
