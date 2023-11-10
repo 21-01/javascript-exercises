14b01300b8c470070089b594b506e62a1832dcee
21-01 <banyaklontelontemaudijual+git@gmail.com>
2023-11-11 03:30:28 +0700

    JavaScript 11-12 Assignments -bad code

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrays) {
+    var results = []
+    for (let index = 0; index < arrays.length; index++) {
+        results[index] = arrays[index].title
+    }
+    return results
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+var findTheOldest = function(arrays) {
+    var tmpObj = {}
+    for (let index = 0; index < arrays.length; index++) {
+        if (!("yearOfDeath" in arrays[index])) arrays[index].yearOfDeath = 2023
+        var name = arrays[index].name
+        var age = arrays[index].yearOfDeath - arrays[index].yearOfBirth
+        tmpObj[name] = age
+    }
+    console.log(tmpObj)
+    var sorted = []
+    for (name in tmpObj){
+        sorted.push([name, tmpObj[name]])
+    }
+    
+    sorted.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    console.log(sorted)
+    return sorted[0][0]
 };
+//findTheOldest(people)
 
 // Do not edit below this line
 module.exports = findTheOldest;
